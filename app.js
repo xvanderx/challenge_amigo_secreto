8d868c9d7f0fada71f13be49049cb5879f47eaec
vanderson <vandersonsilva334@gmail.com>
2025-02-11 15:38:20 -0300

    primeira versao

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os nomes dos amigos
+let amigos = [];
+let listaAmigos = [];
+function adicionarAmigo() {
+    // Obtém o campo de entrada pelo ID
+    let amigo = document.getElementById('amigo');
+    let valor = amigo.value.trim(); // Remove espaços em branco no início e no fim
+
+    // Verifica se o valor não está vazio
+    if (valor !== '') {
+        // Adiciona o nome à array
+        amigos.push(valor);
+
+        // Atualiza a lista de amigos no HTML
+         atualizarListaAmigos();
+
+        // Limpa o campo de entrada
+        amigo.value = '';
+    } else {
+        alert('Por favor, digite um nome válido.');
+    }
+}
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
 
 
 
